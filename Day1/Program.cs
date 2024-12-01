fab806dad952da6ca879ea2467883b20a71472f9
Reyna <reynafox@zohomail.eu>
2024-12-01 21:51:12 +0100

    Day 1.

---
+++ Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿//part1();
+part2();
+
+static void part1() {
+	var list1 = new List<int>();
+	var list2 = new List<int>();
+
+	foreach (var line in File.ReadLines("input.txt")) {
+		var parts = line.Split(' ');
+		list1.Add(int.Parse(parts[0]));
+		list2.Add(int.Parse(parts[^1]));
+	}
+
+	list1.Sort();
+	list2.Sort();
+
+	var sum = 0;
+	for (int i = 0; i < list1.Count; i++) {
+		sum += Math.Abs(list1[i] - list2[i]);
+	}
+
+	Console.WriteLine(sum);
+}
+
+static void part2() {
+	var list = new List<int>();
+	var counts = new Dictionary<int, int>();
+
+	foreach (var line in File.ReadLines("input.txt")) {
+		var parts = line.Split(' ');
+		list.Add(int.Parse(parts[0]));
+		var right = int.Parse(parts[^1]);
+		if (!counts.ContainsKey(right)) counts[right] = 0;
+		counts[right]++;
+	}
+
+	var sum = 0;
+	foreach (var num in list) {
+		sum += num * counts.GetValueOrDefault(num, 0);
+	}
+
+	Console.WriteLine(sum);
+}
